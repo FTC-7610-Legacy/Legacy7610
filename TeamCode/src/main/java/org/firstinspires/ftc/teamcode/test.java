097db853bc5e5c6a284e3e2251394d2985fd7554
jancoding <shahjanvir@gmail.com>
2017-09-15 17:35:34 -0700

    added merge changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -7,17 +7,11 @@ package org.firstinspires.ftc.teamcode;
 
 //Hellow Test File Testing123
 
-<<<<<<< HEAD
 //Janvi is in
 //hellloooooooooo
-=======
 public class test {
 
 }
 
 
-
-
-
 //RANDOM THINGS
->>>>>>> 0703140464ad8529692c9d651f0e870fabf6d47d
